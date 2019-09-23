251ab4c591af09582796514f2c2d5b889f12f2b4
BlazarQSO <BlazarQSO@gmail.com>
2019-09-23 13:12:09 +0300

    brackets/feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 == 1) {
+        return false;
+    }
+
+    let brackets = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        brackets.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+
+    let length = str.length;
+    for (let i = 0; i < length / 2; i++) {
+        for (let j = 0; j < brackets.length; j++) {
+            str = str.replace(brackets[j], "");
+        }
+        if (str.length == 0) {
+            return true;
+        }
+    }
+
+    return false;
 }
